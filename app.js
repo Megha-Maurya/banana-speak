a42d244c6c2d54bbced658563269be0160004787
Megha-Maurya <mauryamegha6@gmail.com>
2022-02-25 14:38:08 +0530

    output

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var btnTranslate = document.querySelector("#btn-translate");
-
+var btnTranslate = document.querySelector("#btn-translator");
 var txtInput = document.querySelector("#txt-input");
-console.log(txtInput);
+var outputDiv = document.querySelector("#output");
 
-function clickHandler() {
-    console.log("clicked");
-    console.log("input", txtInput.value);
+
+function clickHandler(){
+    outputDiv.innerText = ("asjkldjdjl " + txtInput.value);
 }
-btnTranslate.addEventListener("click", clickHandler)
+
+btnTranslate.addEventListener("click", clickHandler);
